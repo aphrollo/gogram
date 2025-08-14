c4569f408c021fd6b24d36dc0807c2dda035f1a3
sprayKING <trickstertwo@gmail.com>
2025-08-15 01:52:39 +0200

    zerolog for logging

--- go.mod
+++ go.mod
@@ -4,5 +4,12 @@ go 1.24.0
 
 require (
 	github.com/pkg/errors v0.9.1
+	github.com/rs/zerolog v1.34.0
 	golang.org/x/net v0.42.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.34.0 // indirect
 )
--- internal/utils/logging.go
+++ internal/utils/logging.go
@@ -1,45 +1,32 @@
-// Copyright (c) 2024 RoseLoverX
-
 package utils
 
 import (
 	"fmt"
-	"log"
+	"os"
 	"runtime"
 	"strings"
+	"time"
+
+	"github.com/rs/zerolog"
 )
 
 type LogLevel int
 
 const (
-	// DebugLevel is the lowest level of logging
 	DebugLevel LogLevel = iota + 1
-	// InfoLevel is the second lowest level of logging (default)
 	InfoLevel
-	// WarnLevel is the third highest level of logging
 	WarnLevel
-	// ErrorLevel is the highest level of logging
 	ErrorLevel
-	// NoLevel disables all logging
 	NoLevel
-	// TraceLevel is the highest level of logging
 	TraceLevel
 )
 
-var (
-	colorOff    = []byte("\033[0m")
-	colorRed    = []byte("\033[0;31m")
-	colorGreen  = []byte("\033[0;32m")
-	colorOrange = []byte("\033[0;33m")
-	colorPurple = []byte("\033[0;35m")
-	colorCyan   = []byte("\033[0;36m")
-)
-
-// Logger is the logging struct.
+// Logger wraps zerolog.Logger but keeps the same API
 type Logger struct {
 	Level   LogLevel
 	Prefix  string
 	nocolor bool
+	zlog    zerolog.Logger
 }
 
 // NoColor disables colorized output.
@@ -49,81 +36,104 @@ func (l *Logger) NoColor(nocolor ...bool) *Logger {
 	} else {
 		l.nocolor = true
 	}
-
+	l.configureLogger()
 	return l
 }
 
-// Color enables colorized output. (default)
+// Color returns true if colorized output is enabled.
 func (l *Logger) Color() bool {
 	return !l.nocolor
 }
 
 func (l *Logger) SetPrefix(prefix string) *Logger {
 	l.Prefix = prefix
+	l.configureLogger()
 	return l
-}
-
-func (l *Logger) colorize(color []byte, s string) string {
-	if l.nocolor {
-		return s
-	}
-	return string(color) + s + string(colorOff)
 }
 
 func (l *Logger) Lev() LogLevel {
 	return l.Level
 }
 
-// SetLevelString sets the level string
 func (l *Logger) SetLevel(level LogLevel) *Logger {
 	l.Level = level
+	l.configureLogger()
 	return l
 }
 
-// Log logs a message at the given level.
 func (l *Logger) Error(v ...any) {
-	// TODO: runtime.Caller(1)
 	if l.Level <= ErrorLevel {
-		log.Println(l.colorize(colorRed, "[error]"), l.Prefix, "-", getVariable(v...))
+		l.zlog.Error().Str("prefix", l.Prefix).Msg(getVariable(v...))
 	}
 }
 
 func (l *Logger) Warn(v ...any) {
 	if l.Level <= WarnLevel {
-		log.Println(l.colorize(colorOrange, "[warn] "), l.Prefix, "-", getVariable(v...))
+		l.zlog.Warn().Str("prefix", l.Prefix).Msg(getVariable(v...))
 	}
 }
 
 func (l *Logger) Info(v ...any) {
 	if l.Level <= InfoLevel {
-		log.Println(l.colorize(colorGreen, "[info] "), l.Prefix, "-", getVariable(v...))
+		l.zlog.Info().Str("prefix", l.Prefix).Msg(getVariable(v...))
 	}
 }
 
 func (l *Logger) Debug(v ...any) {
 	if l.Level <= DebugLevel {
-		log.Println(l.colorize(colorPurple, "[debug]"), l.Prefix, "-", getVariable(v...))
+		l.zlog.Debug().Str("prefix", l.Prefix).Msg(getVariable(v...))
 	}
 }
 
 func (l *Logger) Trace(v ...any) {
 	if l.Level <= TraceLevel {
-		log.Println(l.colorize(colorCyan, "[trace]"), l.Prefix, "-", getVariable(v...))
+		l.zlog.Trace().Str("prefix", l.Prefix).Msg(getVariable(v...))
 	}
 }
 
 func (l *Logger) Panic(v ...any) {
 	stack := make([]byte, 2048)
 	runtime.Stack(stack, false)
-
-	log.Println(l.colorize(colorCyan, "[panic]"), l.Prefix, "-", getVariable(v...), "\n", l.colorize(colorOrange, string(stack)))
+	l.zlog.Panic().
+		Str("prefix", l.Prefix).
+		Str("stack", string(stack)).
+		Msg(getVariable(v...))
 }
 
-// NewLogger returns a new Logger instance.
 func NewLogger(prefix string) *Logger {
-	return &Logger{
+	l := &Logger{
 		Prefix: prefix,
+		Level:  InfoLevel,
 	}
+	l.configureLogger()
+	return l
+}
+
+func (l *Logger) configureLogger() {
+	var output zerolog.ConsoleWriter
+	output.Out = os.Stdout
+	output.TimeFormat = time.RFC3339
+	output.NoColor = l.nocolor
+
+	z := zerolog.New(output).With().Timestamp().Logger()
+
+	// Map our custom LogLevel to zerolog Level
+	switch l.Level {
+	case DebugLevel:
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case InfoLevel:
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case WarnLevel:
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case ErrorLevel:
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	case TraceLevel:
+		zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	case NoLevel:
+		zerolog.SetGlobalLevel(zerolog.Disabled)
+	}
+
+	l.zlog = z
 }
 
 func getVariable(v ...any) string {
